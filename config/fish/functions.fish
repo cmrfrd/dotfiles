49de45b37975dadd563cee63cc70e796e71fbc34
cmrfrd <alexanderjcomerford@gmail.com>
2020-03-20 02:16:26 -0400

    add vterm printing config

--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -49,3 +49,17 @@ end
 # 	printf "orig: %d bytes\n" "$origsize";
 # 	printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio";
 # }
+
+
+function vterm_printf;
+    if [ -n "$TMUX" ]
+        # tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
