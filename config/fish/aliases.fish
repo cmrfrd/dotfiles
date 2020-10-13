b3329ccf8e0a98700c7a917fc0fa703ee304c8b1
cmrfrd <alexanderjcomerford@gmail.com>
2020-10-12 22:51:26 -0400

    Add aliases

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,13 +1,4 @@
-alias dotfiles="cd ~/.dotfiles"
-
-alias a="alsamixer"
-alias c="clear"
-alias e="set -l IN_EMACS 1; emacs-26.3;"
-alias ec="set -l IN_EMACS 1; emacsclient -c -a emacs-26.3;"
-alias g='git'
-alias m="make"
-alias k="kubectl"
-alias r="dotreload"
+source ~/.dotfiles/config/fish/aliases.sh
 
 if command -v exa > /dev/null
 	abbr -a ls 'exa'
@@ -22,65 +13,10 @@ else
   alias lr='ls -tRFh'
 end
 
+if command -v docker-compose > /dev/null
+	abbr -a dc 'docker-compose'
+end
+
 if test -f ~/.nix-profile/share/autojump/autojump.fish;
     source ~/.nix-profile/share/autojump/autojump.fish
 end
-
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
-alias ga='git add'
-alias gaa='git add --all'
-alias gapa='git add --patch'
-alias gau='git add --update'
-alias gav='git add --verbose'
-alias gap='git apply'
-
-alias gb='git branch'
-alias gba='git branch -a'
-alias gbd='git branch -d'
-alias gbD='git branch -D'
-alias gbl='git blame -b -w'
-alias gbnm='git branch --no-merged'
-alias gbr='git branch --remote'
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
-
-alias gc='git commit -v'
-alias gc!='git commit -v --amend'
-alias gcn!='git commit -v --no-edit --amend'
-alias gca='git commit -v -a'
-alias gca!='git commit -v -a --amend'
-alias gcan!='git commit -v -a --no-edit --amend'
-alias gcans!='git commit -v -a -s --no-edit --amend'
-alias gcam='git commit -a -m'
-alias gcsm='git commit -s -m'
-alias gcb='git checkout -b'
-alias gcf='git config --list'
-alias gcl='git clone --recurse-submodules'
-alias gclean='git clean -id'
-alias gcm='git checkout master'
-alias gcd='git checkout develop'
-alias gcmsg='git commit -m'
-alias gco='git checkout'
-alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
-alias gcpa='git cherry-pick --abort'
-alias gcpc='git cherry-pick --continue'
-alias gcs='git commit -S'
-
-alias gst='git status'
-
-alias gd='git diff'
-alias gdca='git diff --cached'
-alias gdcw='git diff --cached --word-diff'
-alias gds='git diff --staged'
-alias gdt='git diff-tree --no-commit-id --name-only -r'
-alias gdw='git diff --word-diff'
