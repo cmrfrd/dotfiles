031aa691d60a5e54099575f2fd4b0daffee2b78d
cmrfrd <alexanderjcomerford@gmail.com>
2020-10-26 18:39:53 -0400

    Add fish reload function

--- config/fish/rc.fish
+++ config/fish/rc.fish
@@ -1,7 +1,11 @@
+function fishreload
+    fish -c fisher
+end
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    fishreload
 end
 
 function dotreload
