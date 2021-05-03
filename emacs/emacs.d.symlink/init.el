c0a82c9a92d44001c4de3185ab861510cd76fd30
cmrfrd <alexanderjcomerford@gmail.com>
2021-05-03 18:49:28 -0400

    Add native comp
    more comments
    
    load config from nixos

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -5,8 +5,16 @@
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
-(package-refresh-contents)
 (setq package-check-signature nil)
+
+;; Native compilation in emacs
+(if (and (fboundp 'native-comp-available-p)
+       (native-comp-available-p))
+    (progn
+      (message "Native compilation is available")
+      (setq comp-deferred-compilation t))
+(message "Native complation is *not* available"))
+
 
 ;; Ensure that use-package is installed.
 ;;
@@ -14,10 +22,20 @@
 ;; fresh installation! So we'll want to update the package repository and
 ;; install use-package before loading the literate configuration.
 (when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
   (package-install 'use-package))
 
+;; Load packages from nixos/home-manager
+(load-file "~/.emacs.d/load-path.el")
+
+;; Load personal config and ensure no prompt per eval
+(setq org-confirm-babel-evaluate nil)
 (org-babel-load-file "~/.emacs.d/mainconfig.org")
+
+;; Start daemon server
 (server-start)
+
+;; Setup tabs
 (elscreen-start)
 (elscreen-tab-mode)
 (elscreen-tab-set-position 'top)
