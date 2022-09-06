c055ccc9c3ce3d961f36b856a6a89a814154443a
cmrfrd <alex@taoa.io>
2022-09-06 13:12:48 -0400

    update org mode elisp packages

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 (setq package-check-signature nil)
 (setq warning-minimum-level :emergency)
